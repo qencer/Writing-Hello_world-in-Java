04b19df558cc784402e4d3308f59edf408624452
qencer <qencer@users.noreply.github.com>
2023-03-02 16:48:43 +0300

    Writing-Hello_world_in_Java
    The first command that every software developer makes as a startup in the software world.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");  //System.out: Process the output of the system,
+                                            // print: Print the text in quotation marks,
+                                            // ln(line): as a line
+   }
+}
